3c080a11beaa7d19a379713998ff97e81e49b190
daiweibao <497266156@qq.com>
2024-02-21 20:49:27 +0800

    添加工具类-网络请求

---
+++ entry/src/main/ets/pages/HttpPage/Model/ShopListModel.ts
@@ -0,0 +1,21 @@
+//数据模型（导出）
+export default class ShopListModel {
+  //id
+  id: number
+  //标题
+  name: string = ''
+  //描述
+  content: string = ''
+  //图片
+  image: string = ''
+  //接口字段
+  weatherinfo: weatherinfo
+}
+class weatherinfo{
+  //风向
+  WD:string = ''
+  //天气
+  weather:string = ''
+}
+
+// 请求结果:"{\"weatherinfo\":{\"SD\":\"92%\",\"WD\":\"东北偏北风\",\"WS\":\"2级\",\"WSE\":\"\",\"city\":\"\",\"cityid\":\"101040100\",\"isRadar\":\"1\",\"radar\":\"JC_RADAR_AZ9010_JB\",\"temp\":\"13\",\"time\":\"09:30\",\"weather\":\"多云\"}}"
